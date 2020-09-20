0327c3e506ac9bfd51d71a6b89c81341c60e560d
Crystal Norman <cnorm88@gmail.com>
2020-09-20 20:51:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  binding.pry
+
   name_hash.collect do |key,value|
     key <=> key
   end
