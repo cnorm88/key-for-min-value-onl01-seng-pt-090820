8652f95387ed177f579a9b0fa1509e91fc70b949
Crystal Norman <cnorm88@gmail.com>
2020-09-20 21:03:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,16 +18,16 @@ require 'pry'
 
 def key_for_min_value(name_hash)
 
-	res = nil
-	value_res = nil
+	min = nil
+	value_min = nil
 	name_hash.each do |key, value|
-		if res == nil
-			res = key
-			value_res = value
-		elsif value < value_res
-			res = key
-			value_res = value  
+		if min == nil
+			min = key
+			value_min = value
+		elsif value < value_min
+			min = key
+			value_min = value  
 		end 
 	end
-	res 
+	min
 end
